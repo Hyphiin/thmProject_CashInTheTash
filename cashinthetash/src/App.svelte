880bc2de81129ab0dcf66976646ffb337b3e933f
Felix Wegener <felix.wegener@iem.thm.de>
2020-12-18 14:45:09 +0100

    Mit Full Hero Block und Fixed Nav

--- cashinthetash/src/App.svelte
+++ cashinthetash/src/App.svelte
@@ -7,45 +7,56 @@
 </svelte:head>
 
 <main>
-    <section class="hero is-primary is-fullheight">
-        <!-- Hero head: will stick at the top -->
-        <div class="hero-head">
-            <nav class="navbar">
-                <div class="container">
-                    <div class="navbar-brand">
-                        <a class="navbar-item">
-                            <img src="https://images.vexels.com/media/users/3/143188/isolated/preview/5f44f3160a09b51b4fa4634ecdff62dd-money-icon-by-vexels.png" alt="Logo">
-                        </a>
-                        <span class="navbar-item is-bold">
-                            CashInTheTash
-                        </span>
-                        <span class="navbar-burger" data-target="navbarMenuHeroA">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-                    </div>
-                    <div id="navbarMenuHeroA" class="navbar-menu">
-                        <div class="navbar-end">
-                            <a class="navbar-item is-active">
-                                Home
-                            </a>
-                            <a class="navbar-item">
-                                FAQ
-                            </a>
-                            <a class="navbar-item">
-                                Team
-                            </a>
-                            <span class="navbar-item">
-              <a class="button is-primary is-inverted">
-                <span>Login</span>
-              </a>
-            </span>
-                        </div>
+    <nav class="navbar has-shadow is-white">
+        <div class="navbar">
+            <div class="navbar-brand">
+                <a class="navbar-item">
+                    <img src="https://images.vexels.com/media/users/3/143188/isolated/preview/5f44f3160a09b51b4fa4634ecdff62dd-money-icon-by-vexels.png"
+                         alt="Logo">
+                </a>
+                <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">Home</a>
+                    <a class="navbar-item">Profile</a>
+                    <a class="navbar-item">FAQ</a>
+                </div>
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link" href="/documentation/overview/start/">
+                        Team
+                    </a>
+                    <div class="navbar-dropdown is-boxed">
+                        <p class="navbar-item" href="">
+                            Felix
+                        </p>
+                        <p class="navbar-item" href="">
+                            Chiara
+                        </p>
                     </div>
                 </div>
-            </nav>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <p class="control">
+                            <a class="button is-primary">
+								<span>
+                				Login
+              					</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
+    </nav>
+    <section class="hero is-primary is-fullheight-with-navbar">
+        <!-- Hero head: will stick at the top -->
+
 
         <!-- Hero content: will be in the middle -->
         <div class="hero-body">
