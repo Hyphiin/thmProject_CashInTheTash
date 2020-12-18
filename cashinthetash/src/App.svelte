67d51f72054b4b0ddcd44523a3a719379a128f6a
Felix Wegener <felix.wegener@iem.thm.de>
2020-12-18 14:39:46 +0100

    Mit Full Hero Block

--- cashinthetash/src/App.svelte
+++ cashinthetash/src/App.svelte
@@ -7,87 +7,69 @@
 </svelte:head>
 
 <main>
-    <nav class="navbar has-shadow is-white">
-        <div class="navbar">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="javascript:window.location.href=window.location.href">
-                    <img src="https://images.vexels.com/media/users/3/143188/isolated/preview/5f44f3160a09b51b4fa4634ecdff62dd-money-icon-by-vexels.png"
-                         alt="Logo">
-                </a>
-                <div class="navbar-burger burger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </div>
-            <div class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="http://localhost:5000/">Home</a>
-                    <a class="navbar-item">FAQ</a>
-                </div>
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        Team
-                    </a>
-                    <div class="navbar-dropdown is-boxed">
-                        <p class="navbar-item">
-                            Felix Wegener
-                        </p>
-                        <p class="navbar-item">
-                            Chiara Knipprath
-                        </p>
+    <section class="hero is-primary is-fullheight">
+        <!-- Hero head: will stick at the top -->
+        <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <a class="navbar-item">
+                            <img src="https://images.vexels.com/media/users/3/143188/isolated/preview/5f44f3160a09b51b4fa4634ecdff62dd-money-icon-by-vexels.png" alt="Logo">
+                        </a>
+                        <span class="navbar-item is-bold">
+                            CashInTheTash
+                        </span>
+                        <span class="navbar-burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+                    </div>
+                    <div id="navbarMenuHeroA" class="navbar-menu">
+                        <div class="navbar-end">
+                            <a class="navbar-item is-active">
+                                Home
+                            </a>
+                            <a class="navbar-item">
+                                FAQ
+                            </a>
+                            <a class="navbar-item">
+                                Team
+                            </a>
+                            <span class="navbar-item">
+              <a class="button is-primary is-inverted">
+                <span>Login</span>
+              </a>
+            </span>
+                        </div>
                     </div>
                 </div>
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                            <p class="control">
-                                <a class="button is-dark">
-								<span>
-                				Login
-              					</span>
-                                </a>
-                            </p>
-                    </div>
-                </div>
-            </div>
-
+            </nav>
         </div>
-    </nav>
-    <section class="hero is-primary has-text-centered is-bold" >
-        <div class="hero-body" >
-            <div class="container">
+
+        <!-- Hero content: will be in the middle -->
+        <div class="hero-body">
+            <div class="container has-text-centered">
                 <h1 class="title is-1">
                     CashInTheTash
                 </h1>
-                <h6 class="subtitle is-6">
+                <h2 class="subtitle is-6">
                     Plane deine Finanzen!
-                </h6>
+                </h2>
             </div>
         </div>
+
+        <!-- Hero footer: will stick at the bottom -->
+        <div class="hero-foot">
+            <nav class="tabs">
+                <div class="container">
+                    <ul>
+                        <li class="is-active"><a>Overview</a></li>
+                        <li><a>Impressum</a></li>
+                        <li><a>Team</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </section>
-
 </main>
-
-
-<style>
-    main {
-        display: flex;
-        text-align: center;
-        width: 100%;
-        height: 100%;
-        flex-direction: column;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
-</style>
