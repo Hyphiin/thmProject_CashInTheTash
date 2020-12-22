24f1a165f61f40b7c79bd12485f4f035f5718323
Felix Wegener <felix.wegener@iem.thm.de>
2020-12-22 12:13:59 +0100

    Modal etwas angepasst.

--- cashinthetash/src/components/ListeErstellen.svelte
+++ cashinthetash/src/components/ListeErstellen.svelte
@@ -11,7 +11,63 @@
             <button class="delete" aria-label="close"></button>
         </header>
         <section class="modal-card-body">
-            <!-- Content ... -->
+            <div class="field">
+                <label class="label">Titel</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="e.g Miete">
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Betrag</label>
+                <div class="control">
+                    <input class="input" type="number" placeholder="e.g. 750€">
+                </div>
+            </div>
+
+            <div class="control">
+                <div class="select">
+                    <select>
+                        <option>Essen</option>
+                        <option>Miete</option>
+                        <option>Kleidung</option>
+                        <option>Freizeit</option>
+                        <option>Arbeit</option>
+                    </select>
+                </div>
+            </div>
+            <hr/>
+            <div class="control">
+                <div class="select">
+                    <select>
+                        <option>Einmalig</option>
+                        <option>Wiederkehrend</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <label class="radio">
+                        <input type="radio" name="question">
+                        Einnahme
+                    </label>
+                    <label class="radio">
+                        <input type="radio" name="question">
+                        Ausgabe
+                    </label>
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-link">Submit</button>
+                </div>
+                <div class="control">
+                    <button class="button is-link is-light">Cancel</button>
+                </div>
+            </div>
+
         </section>
         <footer class="modal-card-foot">
             <button class="button is-success">Save changes</button>
