2ab212f9aad65dfc2b5a2ab931a4f13b25e85d3f
Felix Wegener <felix.wegener@iem.thm.de>
2020-12-23 17:09:51 +0100

    Updaten von Einträgen funktioniert auch.

--- cashinthetash/src/components/Finance.svelte
+++ cashinthetash/src/components/Finance.svelte
@@ -8,14 +8,21 @@
         db.collection('finance').doc(id).delete()
     }
 
+    const updateFinance = () => {
+        db.collection('finance').doc(id).update({
+            Betrag : finance.Betrag,
+            Kategorie : finance.Kategorie
+        })
+    }
+
 </script>
 
 <section>
     <div class="column">
         <div class="notification is-info">
             <h2 class="subtitle has-text-centered">Erstellungsdatum: {finance.Datum}</h2>
-            <input type="text" value={finance.Betrag}/>
-            <input type="text" value={finance.Kategorie}/>
+            <input on:input={updateFinance} type="text" bind:value={finance.Betrag}/>
+            <input on:input={updateFinance} type="text" bind:value={finance.Kategorie}/>
             <button on:click={deleteFinance}>Delete</button>
         </div>
     </div>
